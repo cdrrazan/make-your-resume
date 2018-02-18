106494c310fbbb6a22ca92fdbdd120ed7997bfa3
RA Zan <cdrrazan11@gmail.com>
2018-02-19 04:18:56 +0545

    feature enhancement

---
+++ db/migrate/20180218142818_create_educations.rb
@@ -0,0 +1,12 @@
+class CreateEducations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :educations do |t|
+      t.string :level
+      t.string :institute
+      t.string :year
+      t.string :percentage
+
+      t.timestamps
+    end
+  end
+end
