53c898049ec8d9838d3fc9d12e72060838fe8be3
RA Zan <cdrrazan11@gmail.com>
2018-02-19 06:39:36 +0545

    education details operation complete

--- app/models/cvm.rb
+++ app/models/cvm.rb
@@ -1,7 +1,8 @@
 class Cvm < ApplicationRecord
 
-  validates :fname, :lname, :street, :district,:country, :land,
-            :mobile, :email, :website, :level, :institute, :year,
-            :percentage, :skill, :project, :interest, presence: true
+validates :fname, :lname, :street, :district,:country, :land,
+            :mobile, :email, :website, :institute, :year, :percentage,
+            :hinstitute, :hyear, :hpercentage,
+            :skill, :project, :interest, presence: true
 
 end
