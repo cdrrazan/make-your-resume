293186c835ab24bb9901b3ce467a298308496192
Rajan Bhattarai <cdrrazan@gmail.com>
2018-02-19 14:08:03 +0545

    User module initiation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219003135) do
+ActiveRecord::Schema.define(version: 20180219060627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20180219003135) do
     t.string "percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "cvm_id"
+    t.index ["cvm_id"], name: "index_educations_on_cvm_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -82,4 +84,5 @@ ActiveRecord::Schema.define(version: 20180219003135) do
   end
 
   add_foreign_key "cvms", "users"
+  add_foreign_key "educations", "cvms"
 end
