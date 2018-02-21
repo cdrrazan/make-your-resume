0737aa68814c9491e6aec5341f473845794ae05e
Rajan Bhattarai <cdrrazan@gmail.com>
2018-02-21 16:29:39 +0545

    View Template Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'cvms#index'
     resources :cvms
-    resources :users
+    resources :users, only: [:show]
   get 'about' => 'static_pages#about'
-  get 'users' => 'users#show'
+  get 'user' => 'cvms#show'
   get 'contact' => 'static_pages#contact'
 end
