13d8ceb27d31740b71d9db50c6bf2e3fdd838e66
RA Zan <cdrrazan11@gmail.com>
2018-02-23 15:55:51 +0545

    Validation and Form Layout

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -7,5 +7,5 @@ class Experience < ApplicationRecord
   accepts_nested_attributes_for :projects, :allow_destroy => true, :reject_if => :all_blank
 
 
-  validates :company, :position, :jobdetail, :startdate, :enddate, presence: true
+  validates :company, :position, :jobdetail, :startdate, presence: true
 end
