75afd2393c0995fc0b505026009bae306e465132
Rajan Bhattarai <cdrrazan@gmail.com>
2018-02-24 23:18:02 +0545

    Minor changes and bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  mount RailsAdmin::Engine => '/rails-admin-conf', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/rails-adminx-conf', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'cvms#index'
     resources :cvms
     resources :users, only: [:show]
   get 'about' => 'static_pages#about'
-  get 'cv' => 'cvms#show'
+  get 'my_cv' => 'cvms#show'
   get 'contact' => 'static_pages#contact'
 end
