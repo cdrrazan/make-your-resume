a4015e0ef1e884e6e0683b4ceabf53495dfd2b48
Rajan Bhattarai <cdrrazan@gmail.com>
2018-02-25 11:35:24 +0545

    bash script to automate app setup

---
+++ make-your-cv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#########################################################################################
+# Author    :  Rajan Bhattarai                                                          #
+# Website   :  https://cdrrazan.com                                                     #
+#########################################################################################
+
+#Description
+#Script to automate setting up rails project
+
+#Gem installation for rails project
+bundle install
+
+# Checking and  Creating rails database for local environment
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+
+#Checking if rails default port is used by another application \
+
+touch tmpfile
+netstat -tulpn  | grep :3000 > tmpfile
+
+if [ -s proclisten.txt ]
+  then
+  rails server -p 3535
+else
+  rails server
+fi
+#remove tmpfile
+rm tmpfile
