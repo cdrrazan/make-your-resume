6241478f9369d02ae4818a478f06b5689c705a5a
Rajan Bhattarai <cdrrazan@gmail.com>
2018-04-04 11:01:28 +0545

    admin mode and bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  mount RailsAdmin::Engine => '/rails-adminx-conf', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/myc-adminit', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'cvms#show'
     resources :cvms
