046be9604485228b014702bed402542eedc6cd89
Rajan Bhattarai <cdrrazan@gmail.com>
2018-09-15 09:56:18 +0545

    Design and Route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#about'
   devise_for :users
   mount RailsAdmin::Engine => '/myc-adminit', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
