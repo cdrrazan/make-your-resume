ea6042d29e1d94dc22e22fce4be110c6a541a6cf
Rajan Bhattarai <cdrrazan@gmail.com>
2019-01-02 11:08:13 +0545

    Deletion on Nested Attributes fixed for Projects and Education fields

--- app/models/cvm.rb
+++ app/models/cvm.rb
@@ -6,7 +6,7 @@ class Cvm < ApplicationRecord
 
   has_many :experiences, :inverse_of => :cvm, dependent: :destroy
 
-  accepts_nested_attributes_for :educations, :experiences
+  accepts_nested_attributes_for :educations, :experiences, allow_destroy: true
 
   validates     :fname, :lname, :street, :district,:country, :land,
                  :mobile, :email, :website, :skill, :interest,
